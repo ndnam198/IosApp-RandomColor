1914e6d7c50b606ab61da467bd0cd5c67d15e4f5
ndnam198 <ndnam198@gmail.com>
2023-07-05 15:21:10 +0700

    Initial Commit

---
+++ RandomCorlors/RandomCorlorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCorlorsApp.swift
+//  RandomCorlors
+//
+//  Created by Nam Nguyen on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomCorlorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
